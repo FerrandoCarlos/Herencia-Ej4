2dfc6e7fb930d6f086bd583f12efd44de3dfaff4
Ferrando_Carlos <cferrando2015@gmail.com>
2022-11-15 16:43:09 -0300

    Creacion_Clases

---
+++ G9Ej4/src/g9ej4/calculosFormas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package g9ej4;
+
+/**
+ *
+ * @author Carlitos
+ */
+public interface calculosFormas {
+    
+}
