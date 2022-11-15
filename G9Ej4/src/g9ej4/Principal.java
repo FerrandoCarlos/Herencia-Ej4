45602cb1fe7a35471dad498c39ea208c69a55733
Ferrando_Carlos <cferrando2015@gmail.com>
2022-11-15 20:38:44 -0300

    Final

--- G9Ej4/src/g9ej4/Principal.java
+++ G9Ej4/src/g9ej4/Principal.java
@@ -16,14 +16,31 @@ import java.util.Scanner;
  * @author Carlos Ferrando
  */
 public class Principal {
-
+    public static Scanner input = new Scanner(System.in).useDelimiter("\n");
+    
     public static void main(String[] args) {
         //TODO: crear objetos y pasar parametros
-        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        
         Circulo c = new Circulo();
         Rectangulo r = new Rectangulo();
         
         System.out.println("Ingresaremos los datos del la forma geométrica: ");
+        
+        c.setNombreFigura("Circulo");
+        System.out.printf("Ingresar el diametro del %s:",c.getNombreFigura());
+        c.setDiametro(input.nextDouble());
+        System.out.printf("El area del %s es %.3f\n",c.getNombreFigura(),c.calcularArea());
+        System.out.printf("El perimetro del %s es %.3f\n",c.getNombreFigura(),c.calcularPerimetro());
+        System.out.println("********************************************************************");
+        System.out.println("********************************************************************");
+        r.setNombreFigura("Rectangulo");
+        System.out.printf("Ingresar el tamaño de la base del %s: ",r.getNombreFigura());
+        r.setBase(input.nextDouble());
+        System.out.printf("Ingresar el tamaño de la altura del %s: ",r.getNombreFigura());
+        r.setAltura(input.nextDouble());
+        System.out.printf("El area del %s es %.3f\n",r.getNombreFigura(),r.calcularArea());
+        System.out.printf("El perimetro del %s es %.3f\n",r.getNombreFigura(),r.calcularPerimetro());
+        
     }
 
 }
